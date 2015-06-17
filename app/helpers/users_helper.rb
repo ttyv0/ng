b3e61f4a0622a9f7f67c73a7fb6e97b792169deb
ttyv0_o <ttyv0o0o@gmail.com>
2015-06-17 13:15:02 +0500

    rewrited hide_email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,13 @@ module UsersHelper
 		if user_signed_in? && current_user == user
 			user.email
 		else
-			"hide@ma.il"
+			#my-email@example.com -> m******l@example.com
+			split_email = user.email.split("@")
+			size_name_email = split_email[0].size
+			email = split_email[0][0]
+			(size_name_email - 2).times { |_| email += "*" }
+			email += split_email[0][size_name_email - 1]
+			email + "@" + split_email[1]
 		end
 	end
 end
