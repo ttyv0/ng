0f90c036bddd540ece7c5552a1792a7ca300b9bf
ttyv0_o <ttyv0o0o@gmail.com>
2015-06-19 03:56:16 +0500

    fixed sign_in with :name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,10 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
+
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_in) << :name
-		devise_parameter_sanitizer.for(:sign_up) << :name
+		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
+		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :password, :remember_me) }
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
 	end
 end
