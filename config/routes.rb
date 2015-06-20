588bb83a76985838f6444e92886257eec3cb9ab5
ttyv0_o <ttyv0o0o@gmail.com>
2015-06-21 01:18:11 +0500

    Tabs vs Spaces. Spaces won!

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-	get '/users/:id' => 'users#show', as: 'users'
-	resources :posts, :path => '/' do
-		post 'comment' => 'posts#create_comment'
-		get 'comment/:id' => 'posts#edit_comment'
-		patch 'comment/:id' => 'posts#update_comment'
-		delete 'comment/:id' => 'posts#destroy_comment'
-	end
+  get '/users/:id' => 'users#show', as: 'users'
+  resources :posts, :path => '/' do
+    post 'comment' => 'posts#create_comment'
+    get 'comment/:id' => 'posts#edit_comment'
+    patch 'comment/:id' => 'posts#update_comment'
+    delete 'comment/:id' => 'posts#destroy_comment'
+  end
 end
